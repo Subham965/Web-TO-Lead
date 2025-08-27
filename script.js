ef8e98b10c48508078b6ef58829ecaa8f7e1c15e
subham <subham.choudhary965@gmail.com>
2025-08-27 18:18:14 +0530

    Adding reCaptcha

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function beforeSubmit(){
+let captchachecked = false;
+function beforeSubmit(event){
+if(captchachecked){
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log("inputdate.value",inputdate.value);//string --> date (en_US)
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
     outputdate.value = formattedDate;
+}else{
+    alert("Please check the reCaptch Box to verify that you are human");
+    event.preventDefault();
+}
+    
+}
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+function captchasuccess(){
+    captchachecked = true;
 }
