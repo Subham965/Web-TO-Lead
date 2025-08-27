bc801944e2f35d93307352492e9ef30dd2f523d8
subham <subham.choudhary965@gmail.com>
2025-08-27 16:20:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function beforeSubmit(){
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate.value",inputdate.value);//string --> date (en_US)
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
+    outputdate.value = formattedDate;
+}
